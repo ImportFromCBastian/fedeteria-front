fb12bd58208394012486669cfcce26e36b88ac77
ImportFromCBastian <sebshndz2001@gmail.com>
2024-05-03 14:34:29 -0300

    Fix bug with eslint (further improve)

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,4 @@
-import { browser } from 'globals'
-import pluginJs from '@eslint/js'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
-
 export default [
-  { languageOptions: { globals: browser } },
-  pluginJs.configs.recommended,
-  pluginReactConfig,
   {
     parserOptions: {
       ecmaVersion: 'latest',
