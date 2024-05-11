00bfad50a404e7c3ba9a33984f9cbeb5f700fc7a
Ignacio <meloignacionicolas@gmail.com>
2024-05-11 16:08:06 -0300

    front aceptar o denegar publicacion en estado de aceptacion andando correctamente, se le agrego front y algun chequeo para solo aceptar numeros

--- src/components/Listado-Pub-Aceptacion/Publicacion.jsx
+++ src/components/Listado-Pub-Aceptacion/Publicacion.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
+import { toast, Toaster } from 'sonner'
 export const Publicacion = ({ publicationName, onDelete, onAccept }) => {
   const [showInputModal, setShowInputModal] = useState(false)
   const [numero, setNumero] = useState('')
+  const exceptThisSymbols = ['e', 'E', '+', '-', ',']
 
   const handleChangeNumero = (event) => {
-    setNumero(event.target.value)
+    if (event.target.value === '-' || event.target.value === 'e') {
+      setNumero('')
+    } else setNumero(event.target.value)
   }
 
   const handleSubmit = () => {
     // Lógica para enviar el número y el ID de la publicación al backend
-    onAccept(numero)
+    if (!isNaN(parseInt(numero))) {
+      onAccept(numero)
+    } else {
+      toast.error('Ingrese un numero valido!')
+    }
     // Cerrar la ventana flotante después de enviar el número
     setShowInputModal(false)
   }
@@ -21,6 +29,7 @@ export const Publicacion = ({ publicationName, onDelete, onAccept }) => {
 
   return (
     <div className="relative">
+      <Toaster />
       <div className="rounded-lg bg-white p-4 shadow-md transition-transform duration-300 hover:scale-105 hover:shadow-xl">
         <h3 className="mb-2 text-lg font-medium">{publicationName}</h3>
         <div className="flex items-center justify-end space-x-2">
@@ -43,24 +52,34 @@ export const Publicacion = ({ publicationName, onDelete, onAccept }) => {
       {showInputModal && (
         <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center">
           <div className="absolute left-0 top-0 h-full w-full bg-black bg-opacity-50"></div>
-          <div className="z-10 rounded-lg bg-white p-8">
-            <h2 className="mb-4 text-lg font-semibold">Ingrese un número</h2>
+          <div className="z-10 w-96 rounded-lg bg-white p-8">
+            <h2 className="mb-4 text-lg font-semibold">Ingrese precio para la publicacion</h2>
             <input
               type="number"
-              value={numero}
+              autoComplete="off"
               onChange={handleChangeNumero}
-              className="mb-4 w-full rounded-md border border-gray-300 px-3 py-2"
-              placeholder="Ingrese un número"
+              onKeyDown={(e) => exceptThisSymbols.includes(e.key) && e.preventDefault()}
+              className="mb-4 w-full rounded-md border border-gray-300 px-3 py-2 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+              placeholder="Ingrese el precio"
             />
-            <button
-              onClick={handleSubmit}
-              className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-            >
-              Enviar
-            </button>
-            <button onClick={handleModalClose} className="ml-2 text-gray-600 hover:text-gray-800">
-              Cerrar
-            </button>
+            <div className="flex w-full justify-start space-x-4">
+              <button
+                onClick={handleSubmit}
+                className="ring-offset-background focus-visible:ring-ring border-input bg-background inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border px-3 text-sm font-medium transition-colors hover:scale-105 hover:bg-green-500 hover:text-white focus-visible:outline-none focus-visible:ring-2  focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+              >
+                Enviar
+                <span className="material-symbols-outlined pl-1.5">check</span>
+              </button>
+
+              <button
+                onClick={handleModalClose}
+                className="ring-offset-background focus-visible:ring-ring border-input bg-background inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border px-3 text-sm font-medium transition-colors hover:scale-105 hover:bg-red-500 hover:text-white focus-visible:outline-none focus-visible:ring-2  focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+                style={{ marginLeft: '8px' }} // Agregar un margen izquierdo para separar los botones
+              >
+                Cancelar
+                <span className="material-symbols-outlined pl-1.5">close</span>
+              </button>
+            </div>
           </div>
         </div>
       )}
