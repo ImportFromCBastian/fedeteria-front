e199f7f1c180bbd780dbec0574392be593cb4705
Ignacio <meloignacionicolas@gmail.com>
2024-05-12 16:59:43 -0300

    falta conseguir dni por token y front

--- src/components/Perfil/ConseguirPerfil.jsx
+++ src/components/Perfil/ConseguirPerfil.jsx
@@ -3,17 +3,25 @@ import { ShowProfile } from './profile'
 
 function MostrarPerfil() {
   const [perfil, setPerfil] = useState('')
+  const [error, setError] = useState(false)
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_BASE_URL}/mi-perfil/${44590363}`, {
+    const dniPerfil = 44590363
+    fetch(`${import.meta.env.VITE_BASE_URL}/mi_perfil/${dniPerfil}`, {
       method: 'GET'
     })
       .then((response) => response.json())
       .then((data) => {
         setPerfil(data.data)
       })
-      .catch((error) => console.error('Error al obtener el perfil', error))
+      .catch((error) => {
+        console.error('Error al obtener el perfil', error)
+        setError(true)
+      })
   }, []) // Asegúrate de pasar un array vacío como segundo argumento para que useEffect se ejecute solo una vez al montar el componente
-  return <ShowProfile userData={perfil} />
+  if (error) {
+    return <div>Error al conseguir el perfil!</div>
+  }
+  return <ShowProfile userData={perfil.at(0)} />
 }
 export default MostrarPerfil
--- src/components/Perfil/profile.jsx
+++ src/components/Perfil/profile.jsx
@@ -1,94 +1,106 @@
 export const ShowProfile = ({ userData }) => {
+  const formatFechaNacimiento = (fechaNacimiento) => {
+    if (!fechaNacimiento) return '' // Manejo de caso en que la fecha no esté definida
+
+    const fecha = new Date(fechaNacimiento)
+    const dia = fecha.getDate().toString().padStart(2, '0')
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0')
+    const anio = fecha.getFullYear()
+
+    return `${dia}/${mes}/${anio}`
+  }
   return (
     <div className="flex h-screen items-center justify-center">
-      <div
-        className="bg-card text-card-foreground w-full max-w-2xl rounded-lg border shadow-sm"
-        data-v0-t="card"
-      >
-        <div className="flex flex-col space-y-2 p-6">
-          <h3 className="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight">
-            Hola, {userData.nombre}!
-          </h3>
-          <p className="text-muted-foreground text-sm">Estos son los datos de tu perfil</p>
-          <button className="ring-offset-background focus-visible:ring-ring hover:bg-accent hover:text-accent-foreground ml-auto inline-flex h-7 w-7 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-5 w-5"
-            >
-              <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"></path>
-              <path d="m15 5 4 4"></path>
-            </svg>
-            <span className="sr-only">Editar perfil</span>
-          </button>
-        </div>
-        <div className="space-y-2 p-6">
-          {' '}
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-6">
-            <div className="space-y-2">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="name"
+      {userData && (
+        <div
+          className="bg-card text-card-foreground w-full max-w-2xl rounded-lg border shadow-sm"
+          data-v0-t="card"
+        >
+          <div className="flex flex-col space-y-2 p-6">
+            <h3 className="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight">
+              Hola, {userData.nombre}!
+            </h3>
+            <p className="text-muted-foreground text-sm">Estos son los datos de tu perfil</p>
+            <button className="ring-offset-background focus-visible:ring-ring hover:bg-accent hover:text-accent-foreground ml-auto inline-flex h-7 w-7 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="h-5 w-5"
               >
-                Nombre
-              </label>
-              <div className="font-medium">{userData.nombre}</div>
-            </div>
-            <div className="space-y-2">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="lastName"
-              >
-                Apellido
-              </label>
-              <div className="font-medium">{userData.apellido}</div>
-            </div>
-            <div className="space-y-2">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="email"
-              >
-                Correo electrónico
-              </label>
-              <div className="font-medium">{userData.mail}</div>
-            </div>
-            <div className="space-y-2">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="dni"
-              >
-                DNI
-              </label>
-              <div className="font-medium">{userData.dni}</div>
-            </div>
-            <div className="space-y-2">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="birthDate"
-              >
-                Fecha de nacimiento
-              </label>
-              <div className="font-medium">{userData.fechaNacimiento}</div>
-            </div>
-            <div className="space-y-2">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="branch"
-              >
-                Sucursal más cercana
-              </label>
-              <div className="font-medium">Sucursal 1</div>
+                <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"></path>
+                <path d="m15 5 4 4"></path>
+              </svg>
+              <span className="sr-only">Editar perfil</span>
+            </button>
+          </div>
+          <div className="space-y-2 p-6">
+            {' '}
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-6">
+              <div className="space-y-2">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="name"
+                >
+                  Nombre
+                </label>
+                <div className="font-medium">{userData.nombre}</div>
+              </div>
+              <div className="space-y-2">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="lastName"
+                >
+                  Apellido
+                </label>
+                <div className="font-medium">{userData.apellido}</div>
+              </div>
+              <div className="space-y-2">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="email"
+                >
+                  Correo electrónico
+                </label>
+                <div className="font-medium">{userData.mail}</div>
+              </div>
+              <div className="space-y-2">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="dni"
+                >
+                  DNI
+                </label>
+                <div className="font-medium">{userData.DNI}</div>
+              </div>
+              <div className="space-y-2">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="birthDate"
+                >
+                  Fecha de nacimiento
+                </label>
+                <div className="font-medium">{formatFechaNacimiento(userData.fechaNacimiento)}</div>
+              </div>
+              <div className="space-y-2">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="branch"
+                >
+                  Sucursal más cercana
+                </label>
+                <div className="font-medium">Sucursal 1</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
