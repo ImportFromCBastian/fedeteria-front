2e931bc0f13dfdb70af03387200de941243150c0
Sebastian Hernandez <sebshndz2001@gmail.com>
2024-05-14 18:58:40 -0300

    chore: Remove unused files and code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Login } from './components/Login.jsx'
