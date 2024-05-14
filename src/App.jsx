ff498974974633238c6bb1b09b797862d9808092
Ignacio <meloignacionicolas@gmail.com>
2024-05-14 11:39:23 -0300

    IMPLEMENTE el FRONT, quedan detalles que pulir, e implementaciones que terminar al momento de hacer merge(tokens y validaciones)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // import { PaymentBrick } from './components/payment/PaymentBrick'
 import { RegisterClientForm } from './components/Client/RegisterClientForm'
+import { DetallesPublicacion } from './components/DetallesPublicacion/VistaPublicacion'
 function App() {
   return (
     <>
-      <RegisterClientForm />
+      <DetallesPublicacion />
     </>
   )
 }
