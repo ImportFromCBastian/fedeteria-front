723105addb3f5923f918e6212e428d302088ca4f
Ignacio <meloignacionicolas@gmail.com>
2024-05-29 22:09:15 -0300

    feat + fix: Actualizar formularios de registro, listado y eliminacion de sucursales

--- src/components/Sucursal/hooks/validator/sucursalValidator.jsx
+++ src/components/Sucursal/hooks/validator/sucursalValidator.jsx
@@ -4,8 +4,8 @@ const sucursalSchema = y.object({
   nombre: y.string().required('El nombre es requerido'),
   calle: y.string().required('La calle es requerida'),
   numero: y.string().required('El número es requerido'),
-  piso: y.number().optional(),
-  depto: y.string().optional()
+  piso: y.number().nullable().optional(),
+  depto: y.string().nullable().optional()
 })
 
 export default sucursalSchema
