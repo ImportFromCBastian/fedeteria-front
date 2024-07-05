7e0dc895cd7dcba0f75d4019a938efd6983cc0cb
Sebastian Hernandez <sebshndz2001@gmail.com>
2024-07-05 17:31:48 -0300

    refactor: Update EstablishExchange component to handle different states and improve UI

--- src/components/EstablishExchange/EstablishExchange.jsx
+++ src/components/EstablishExchange/EstablishExchange.jsx
@@ -5,7 +5,7 @@ import { useHandler } from './hooks/useExchangeHandler'
 import { ExchangeSales } from './ExchangeSales'
 import { Toaster } from 'sonner'
 
-// state: cp (code pending) | ace (accept or cancel exchange) | s (registering a sale)
+// state: cp (code pending) | ace (accept or cancel exchange) | rs (registering a sale)
 
 export const EstablishExchange = () => {
   const [code, setCode] = useState('')
@@ -18,10 +18,26 @@ export const EstablishExchange = () => {
     <div className="flex min-h-screen flex-col items-center justify-center bg-[url('/background-pattern.svg')] bg-cover">
       <div className="w-full max-w-2xl rounded-lg bg-white p-8 shadow-lg">
         <Toaster richColors={true} />
-        <h1 className="mb-4 text-center text-3xl font-bold">Determinar realización de trueque</h1>
-        <p className="text-muted-foreground mb-8 text-center">
-          Ingresa el código del trueque asignado para determinar si se realizará o no
-        </p>
+        {state != 'rs' ? (
+          <>
+            <h1 className="mb-4 text-center text-3xl font-bold">
+              Determinar realización de trueque
+            </h1>
+            <p className="text-muted-foreground mb-8 text-center">
+              Ingresa el código del trueque asignado para determinar si se realizará o no
+            </p>
+          </>
+        ) : (
+          <>
+            <h1 className="mb-4 text-center text-3xl font-bold">
+              Realizando venta durante el trueque
+            </h1>
+            <p className="text-muted-foreground mb-8 text-center">
+              Ingresa los productos que los clientes del intercambio desean comprar
+            </p>
+          </>
+        )}
+
         {/* TODO: ADD toaster and toast to show each error in the useHanlder file */}
         {state === 'cp' && (
           <ExchangeCodeForm childSubmit={handleChildSubmit} childChange={handleChildChange} />
